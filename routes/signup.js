218d45677f88a387b89177e25f201c5168ca4ab2
Andrei Gasparovici <andreigasparovici1@gmail.com>
2016-11-26 11:00:48 +0200

    Modified

--- routes/signup.js
+++ routes/signup.js
@@ -1,13 +1,35 @@
 const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const csrf = require('csurf');
+const validator = require('email-validator');
 
 var router = express.Router();
 
-router.get('/',(req,res)=>{
+var csrfProtection = csrf({ cookie: true });
+
+var User = require('../models/user');
+
+router.get('/',csrfProtection,(req,res)=>{
     res.render('signup');
 });
 
-router.post('/',(req,res)=>{
-
+router.post('/',csrfProtection,(req,res)=>{
+    if(!req.body.email || !validator.validate(req.body.email)){
+        req.flash("error","Invalid email!");
+        res.redirect("/signup");
+        return;
+    }
+    if(!req.body.password || !req.body.confirmPassword || req.body.password!=req.body.confirmPassword){
+        req.flash("error","Invalid password!");
+        res.redirect("/signup");
+        return;
+    }
+     User.findOne({
+        'email':req.body.email
+    },(err,user)=>{
+        if(err) throw err;
+        
+    });
 });
 
 module.exports=router;
